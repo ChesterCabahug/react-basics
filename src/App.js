79f22b342682249881faf45fc2a3fe44b9f9dc27
Chester <chesterdeveloper2000@gmail.com>
2020-10-09 14:28:52 +0800

    just trying out map()

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      monsters: [
+        {
+          name: "Frankenstein",
+        },
+        {
+          name: "Dracula",
+        },
+        {
+          name: "Zombie",
+        },
+      ],
+    };
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello World</p>
-        </header>
+        {this.state.monsters.map((monster) => (
+          <h1>{monster.name}</h1>
+        ))}
       </div>
     );
   }
