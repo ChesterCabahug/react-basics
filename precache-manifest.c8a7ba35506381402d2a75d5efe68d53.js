9f0cf2d5db8c070c697c9667933956ecaaa54e02
Chester <chesterdeveloper2000@gmail.com>
2020-10-14 20:56:16 +0800

    Updates

---
+++ precache-manifest.c8a7ba35506381402d2a75d5efe68d53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e701e85d008de64ce3a66cf100322c5e",
+    "url": "/react-basics/index.html"
+  },
+  {
+    "revision": "a6ee7a16d5dc3097f2f7",
+    "url": "/react-basics/static/css/main.37708934.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/react-basics/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basics/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6ee7a16d5dc3097f2f7",
+    "url": "/react-basics/static/js/main.ef2d6bbe.chunk.js"
+  },
+  {
+    "revision": "c8f8275c137178bd9458",
+    "url": "/react-basics/static/js/runtime-main.a5df6b8e.js"
+  }
+]);
